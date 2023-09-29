bd8dcc53726dd31985cfd9ec8f88f28db7d8c8e5
AlejandroLosilla <alejandrolosillagalera@gmail.com>
2023-09-29 16:24:46 +0200

    Configure gh actions

--- src/infrastructure/Shared/config.js
+++ src/infrastructure/Shared/config.js
@@ -1,25 +1,25 @@
 export const config = {
-    mailgun: {
-      domain: "sandbox438c8dd938f0410aa1dd0393b97f4f46.mailgun.org",
-      authUser: "api",
-      apiKey: process.env.API_MAILGUN,
-    },
-    testInbox: {
-      apiKey: process.env.API_TESTMAIL,
-      namespace: "kcat1",
-    },
-    mongo: {
-      user: "admin",
-      password: process.env.MONGO_PASSWORD,
-      address: "localhost",
-      port: "27017",
-    },
-    postgres: {
-      user: "admin",
-      password: process.env.POSTGRES_PASSWORD,
-      host: "localhost",
-      db: "my-project",
-      port: "5431",
-    },
-    redis: {},
-  }
+  mailgun: {
+    domain: "sandbox438c8dd938f0410aa1dd0393b97f4f46.mailgun.org",
+    authUser: "api",
+    apiKey: process.env.API_MAILGUN,
+  },
+  testInbox: {
+    apiKey: process.env.API_TESTMAIL,
+    namespace: "kcat1",
+  },
+  mongo: {
+    user: "admin",
+    password: process.env.MONGO_PASSWORD,
+    address: "localhost",
+    port: "27017",
+  },
+  postgres: {
+    user: "admin",
+    password: process.env.POSTGRES_PASSWORD,
+    host: "localhost",
+    db: "my-project",
+    port: "5431",
+  },
+  redis: {},
+}
